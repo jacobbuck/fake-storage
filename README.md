7c4caabd1bcddd9827abe2839e93f10835201395
Jacob Buck <buck.jacob@gmail.com>
2015-05-02 15:33:06 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+Fake Storage
+============
+
+Memory-based storage to shim [Web Storage](http://www.w3.org/TR/webstorage/).
+
+Documentation
+-------------
+
+Create a new instance:
+
+```js
+var myStorage = new FakeStorage();
+```
+
+Then you can you your `myStorage` object much like the [Storage interface](http://www.w3.org/TR/webstorage/#storage):
+
+```js
+myStorage.getItem('foo');
+
+myStorage.setItem('foo', 'bar');
+
+myStorage.removeItem('foo');
+
+myStorage.key(2);
+
+myStorage.length;
+```
+
+Compatibility
+-------------
+
+There's nothing fancy going on here, so should work everywhere (even IE6).
+
+License
+-------
+
+MIT - see [LICENSE](LICENSE)
---
+++ fake-storage.js
@@ -0,0 +1,68 @@
+/*!
+ * Fake Storage - Copyright (c) 2015 Jacob Buck
+ * https://github.com/jacobbuck/fake-storage
+ * Licensed under the terms of the MIT license.
+ */
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		// CommonJS
+		exports.FakeStorage = factory();
+	} else {
+		// Browser global
+		root.FakeStorage = factory();
+	}
+}(this, function () {
+	'use strict';
+
+	function FakeStorage () {
+		this.clear();
+	}
+
+	FakeStorage.prototype = {
+		key: function (index) {
+			// sanitise index as int
+			index = parseInt(index);
+			// return early if index isn't a number, or is negative
+			if (isNaN(index) || index < 0) { return null; }
+			// get all of the keys
+			var keys = Object.keys(this.__data);
+			// get the key from the index, otherwise return null
+			return (index < keys.length) ? keys[index] : null;
+		},
+
+		getItem: function (key) {
+			// only get if there's something to get
+			return (key !== '' && this.__data.hasOwnProperty(key)) ? this.__data[key] : null;
+		},
+
+		setItem: function (key, value) {
+			// ensure the key isn't empty
+			if (key !== '') {
+				// if we're adding a new item, incriment the length
+				if (!this.__data.hasOwnProperty(key)) {
+					this.length++;
+				}
+				// always store the value as a string
+				this.__data[key] = value.toString();
+			}
+		},
+
+		removeItem: function (key) {
+			// only remove if there's something to remove
+			if (key !== '' && this.__data.hasOwnProperty(key)) {
+				delete this.__data[key];
+				this.length--;
+			}
+		},
+
+		clear: function () {
+			this.__data = {};
+			this.length = 0;
+		}
+	};
+
+	return FakeStorage;
+}));
