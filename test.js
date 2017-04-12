ea4b6f074395104ba5dfa268541de6df3ad7ead8
Jacob Buck <buck.jacob@gmail.com>
2017-04-12 19:43:25 +1200

    Use expect instead of expect.js. Update tests to work.

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-var expect = require('expect.js');
-var FakeStorage = require('../fake-storage.js');
+var expect = require('expect');
+var FakeStorage = require('.');
 
 describe('Fake Storage', function () {
 
@@ -8,53 +8,53 @@ describe('Fake Storage', function () {
 			var store = new FakeStorage();
 			store.setItem('foo', 'bar');
 			store.setItem('', 'bar');
-			expect(store.getItem('foo')).to.be('bar');
-			expect(store.getItem('')).to.be('bar');
+			expect(store.getItem('foo')).toBe('bar');
+			expect(store.getItem('')).toBe('bar');
 		});
 
 		it('returns null if the given key does not exist', function () {
 			var store = new FakeStorage();
-			expect(store.getItem('foz')).to.be(null);
+			expect(store.getItem('foz')).toBe(null);
 		});
 	});
 
 	describe('setItem method', function () {
 		it('stores a new key/value pair', function () {
 			var store = new FakeStorage();
-			expect(store.getItem('foo')).to.be(null);
+			expect(store.getItem('foo')).toBe(null);
 			store.setItem('foo', 'bar');
-			expect(store.getItem('foo')).to.be('bar');
+			expect(store.getItem('foo')).toBe('bar');
 		});
 
 		it('stores the value as a string', function () {
 			var store = new FakeStorage();
 			store.setItem('number', 2);
-			expect(store.getItem('number')).to.be('2');
+			expect(store.getItem('number')).toBe('2');
 			store.setItem('boolean', true);
-			expect(store.getItem('boolean')).to.be('true');
+			expect(store.getItem('boolean')).toBe('true');
 			store.setItem('array', ['hey', 'man']);
-			expect(store.getItem('array')).to.be('hey,man');
+			expect(store.getItem('array')).toBe('hey,man');
 			store.setItem('object', {hey: 'man'});
-			expect(store.getItem('object')).to.be('[object Object]');
+			expect(store.getItem('object')).toBe('[object Object]');
 		});
 
 		it('updates the value of an existing key', function () {
 			var store = new FakeStorage();
 			store.setItem('foo', 'bar');
-			expect(store.getItem('foo')).to.be('bar');
+			expect(store.getItem('foo')).toBe('bar');
 			store.setItem('foo', 'baz');
-			expect(store.getItem('foo')).to.be('baz');
+			expect(store.getItem('foo')).toBe('baz');
 		});
 
 		it('increases the length attribute when storing a new key/value pair', function () {
 			var store = new FakeStorage();
-			expect(store.length).to.be(0);
+			expect(store.length).toBe(0);
 			store.setItem('foo', 'bar');
-			expect(store.length).to.be(1);
+			expect(store.length).toBe(1);
 			store.setItem('foo', 'rad');
-			expect(store.length).to.be(1);
+			expect(store.length).toBe(1);
 			store.setItem('foz', 'baz');
-			expect(store.length).to.be(2);
+			expect(store.length).toBe(2);
 		});
 	});
 
@@ -63,22 +63,22 @@ describe('Fake Storage', function () {
 			var store = new FakeStorage();
 			store.setItem('foo', 'bar');
 			store.removeItem('foo');
-			expect(store.getItem('foo')).to.be(null);
+			expect(store.getItem('foo')).toBe(null);
 		});
 
 		it('decreases the length attribute', function () {
 			var store = new FakeStorage();
 			store.setItem('foo', 'bar');
 			store.removeItem('foo');
-			expect(store.length).to.be(0);
+			expect(store.length).toBe(0);
 		});
 
 		it('does nothing if the given key does not exist', function () {
 			var store = new FakeStorage();
 			store.setItem('foo', 'bar');
 			store.removeItem('foz');
-			expect(store.getItem('foo')).to.be('bar');
-			expect(store.length).to.be(1);
+			expect(store.getItem('foo')).toBe('bar');
+			expect(store.length).toBe(1);
 		});
 	});
 
@@ -89,12 +89,12 @@ describe('Fake Storage', function () {
 		store.clear();
 
 		it('removes all key/value pairs', function () {
-			expect(store.getItem('bar')).to.be(null);
-			expect(store.getItem('foo')).to.be(null);
+			expect(store.getItem('bar')).toBe(null);
+			expect(store.getItem('foo')).toBe(null);
 		});
 
 		it('resets the length attribute', function () {
-			expect(store.length).to.be(0);
+			expect(store.length).toBe(0);
 		});
 	});
 
@@ -104,22 +104,22 @@ describe('Fake Storage', function () {
 		store.setItem('bar', 'ho!');
 
 		it('returns the name of the nth key in the list', function () {
-			expect(store.key(0)).to.be('foo');
-			expect(store.key('0')).to.be('foo');
-			expect(store.key(1)).to.be('bar');
-			expect(store.key(1.0)).to.be('bar');
+			expect(store.key(0)).toBe('foo');
+			expect(store.key('0')).toBe('foo');
+			expect(store.key(1)).toBe('bar');
+			expect(store.key(1.0)).toBe('bar');
 		});
 
 		it('returns null if the given index is greater than or equal to the number of key/value pairs', function () {
-			expect(store.key(store.length)).to.be(null);
-			expect(store.key(9001)).to.be(null);
+			expect(store.key(store.length)).toBe(null);
+			expect(store.key(9001)).toBe(null);
 		});
 
 		it('returns null if the given index is not valid', function () {
-			expect(store.key(-1)).to.be(null);
-			expect(store.key(NaN)).to.be(null);
-			expect(store.key(Infinity)).to.be(null);
-			expect(store.key('foo')).to.be(null);
+			expect(store.key(-1)).toBe(null);
+			expect(store.key(NaN)).toBe(null);
+			expect(store.key(Infinity)).toBe(null);
+			expect(store.key('foo')).toBe(null);
 		});
 	});
 
