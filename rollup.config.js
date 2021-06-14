db450d15ba8b63a236a5d46dc88a765b30c280d4
Jacob Buck <buck.jacob@gmail.com>
2021-06-14 05:47:50 +0000

    Update rollup.config.js to read from package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,11 @@
 import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
   output: [
-    {
-      exports: 'default',
-      file: 'lib/index.cjs.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: 'lib/index.esm.js',
-      format: 'esm',
-      sourcemap: true,
-    },
+    { file: pkg.main, format: 'cjs', sourcemap: true, exports: 'default' },
+    { file: pkg.module, format: 'esm', sourcemap: true },
   ],
   external: [/@babel\/runtime/],
   plugins: [
